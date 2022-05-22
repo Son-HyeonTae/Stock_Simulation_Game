b4d18ec5b664fa2544ac4af656544a0c58fccc2e
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-05-22 18:34:57 +0900

    Add files via upload

---
+++ application/Exchange.java
@@ -0,0 +1,6 @@
+package application;
+
+public class Exchange extends StockGame {
+	
+	String company[] = {"Samsung", "SK Hynix", "Naver", "Hyundai", "Kakao"};
+}
---
+++ application/User.java
@@ -0,0 +1,15 @@
+package application;
+
+public class User extends HTS {
+	
+	public static void main(String[] args) {
+		
+		// create instance
+		StockGame stockgame = new StockGame();
+		HTS hts = new HTS();
+		Stock stock = new Stock();
+		
+ 		int seed = hts.seed;
+ 		int wallet[] = hts.wallet;
+	}
+}
