7ba1111c73ea695145bad970885f752dfcae92e2
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-05-22 16:58:25 +0900

    Delete StockSimul directory

--- StockSimul/src/Exchange.java
+++
@@ -1,6 +0,0 @@
-
-public class Exchange extends StockGame {
-	
-	String company[] = {"", "삼성전자", "SK하이닉스", "NAVER", "현대차", "카카오"};
-	
-}
--- StockSimul/src/StockGame.java
+++
@@ -1,5 +0,0 @@
-
-public class StockGame {
-	// stock value
-	int stock[] = {0, 0, 0, 0, 0, 0};
-}
